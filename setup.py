f49b8d0a6e6137e04554281fc8f8985c8e1a2b74
nonejou072 <jou072@126.com>
2023-09-07 23:45:53 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='robopal',
     packages=[package for package in find_packages() if package.startswith("robopal")],
-    version='0.0.1',
-    author="Haoran Zhou, Yichao Huang, Yuhan Zhao, Yang Lu",
-    author_email="jou072@126.com, 15897778219@163.com, zyhzyh9426@163.com, yanglu202202@163.com",
+    version='0.1.0',
+    author="Haoran Zhou, Yichao Huang, Zongdao Li, Yuhan Zhao, Yang Lu",
+    author_email="jou072@126.com",
     description="robopal: A Simulation Framework Based Mujoco",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/NoneJou072/robopal",
-    python_requires=">=3",
+    python_requires=">=3.9",
     install_requires=[
         "numpy>=1.24.0",
         'mujoco==2.3.7',
         'ruckig~=0.9.2',
+        'pin'
     ],
     extras_require={
         'interactive': ['gymnasium']
     },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
 )
