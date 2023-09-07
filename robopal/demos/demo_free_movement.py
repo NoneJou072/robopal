4d0f1259bef061c35a254644ae1eebd8f3f66778
nonejou072 <jou072@126.com>
2023-09-07 12:57:32 +0800

    add demo1

--- robopal/demos/demo_free_movement.py
+++ robopal/demos/demo_free_movement.py
@@ -1,19 +1,45 @@
+import argparse
 import numpy as np
-from robopal.envs.jnt_ctrl_env import SingleArmEnv
+
 
 if __name__ == "__main__":
-    from robopal.assets.robots.diana_med import DianaMed
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ctrl', type=str, help='JSC for task space controller or OSC for joint space controller')
+    args = parser.parse_args()
 
-    env = SingleArmEnv(
-        robot=DianaMed(),
-        renderer='viewer',
-        is_render=True,
-        control_freq=200,
-        is_interpolate=True,
-    )
-    env.reset()
-    for t in range(int(1e6)):
-        action = np.array([0.33116, -0.39768533, 0.66947228, 0.33116, -0.39768533, 0.66947228, 0])
-        env.step(action)
-        if env.is_render:
-            env.render()
+    if args.env == 'JSC':
+        from robopal.assets.robots.diana_med import DianaMed
+        from robopal.envs import SingleArmEnv
+
+        env = SingleArmEnv(
+            robot=DianaMed(),
+            renderer='viewer',
+            is_render=True,
+            control_freq=200,
+            is_interpolate=True,
+        )
+        env.reset()
+        for t in range(int(1e6)):
+            action = np.array([0.33116, -0.39768533, 0.66947228, 0.33116, -0.39768533, 0.66947228, 0])
+            env.step(action)
+            if env.is_render:
+                env.render()
+
+    elif args.env == 'OSC':
+        from robopal.assets.robots.diana_med import DianaMed
+        from robopal.envs import PosCtrlEnv
+
+        env = PosCtrlEnv(
+            robot=DianaMed(),
+            renderer='viewer',
+            is_render=True,
+            control_freq=200,
+            is_interpolate=True,
+            is_pd=True
+        )
+        env.reset()
+        for t in range(int(1e6)):
+            action = np.array([0.33116, -0.39768533, 0.66947228])
+            env.step(action)
+            if env.is_render:
+                env.render()
